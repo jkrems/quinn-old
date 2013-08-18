5fe0e43727792c00603df9774a25b50b05d81ca0
Jan Krems <jan.krems@groupon.com>
2013-08-18 14:50:57 +0200

    Added lazyMap to new harmony module

---
+++ harmony.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function lazyMap(collection, fn, bufferSize) {
+  if (null == bufferSize) { bufferSize = 1; }
+
+  function* iterate() {
+    let key, inputQueue = [], resultQueue = [];
+    for (key in collection) {
+      inputQueue.push(key);
+    }
+
+    let fillUpQueue = function() {
+      // make sure resultQueue has at least bufferSize + 1 elements
+      let diff = Math.max(0, (bufferSize - resultQueue.length + 1));
+      let newResults = inputQueue.splice(0, diff).map(function(key) {
+        return fn(collection[key], key, collection);
+      });
+      // add the new results to the end of the result queue
+      resultQueue = resultQueue.concat(newResults);
+    };
+    fillUpQueue();
+
+    while (resultQueue.length > 0) {
+      let mapped = resultQueue.shift();
+      fillUpQueue();
+      yield mapped;
+    }
+  };
+  // collection may be:
+  // a) an array
+  // b) an object
+  // c) a generator
+  let cType = Object.prototype.toString.call(collection).slice(8, -1);
+  if (Array.isArray(collection)) {
+    return iterate();
+  } else if (cType === 'Generator') {
+    // TODO: handle Generator
+  } else if (typeof collection === 'object') {
+    return iterate();
+  }
+  throw new Error(
+    "Unsupported type of collection for lazyMap: " + typeof collection
+  );
+};
+
+module.exports = {
+  lazyMap: lazyMap
+};
